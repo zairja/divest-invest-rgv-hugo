f0b4eca1d9a2574f37f76554e0428165b969cb4a
Zairja <judeau@gmail.com>
2023-10-05 20:22:29 -0500

    update nav and jumbotron

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -4,9 +4,11 @@ export default class Jumbotron extends React.Component {
   render() {
     const {image, title, subtitle} = this.props;
     return <div>
-      <div className="pv5 pv6-l ph3 bg-center cover" style={{
-        backgroundImage: image && `url(${image})`
-      }}>
+      <div className="pv5 pv6-l ph3 bg-center contain" style={{
+        backgroundImage: image && `url(${image})`,
+        backgroundColor: '#fffbe2'
+      }}
+      >
         <div className="mw7 center ph3">
           <div className="db mb3">
             <div className="mw7 relative bg-fix-primary mb3">
